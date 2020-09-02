18e5397574578fce7555cd23709463a9e02f8e45
dimitriRemoiville <dimitri.remoiville@gmail.com>
2020-09-02 19:46:07 +1000

    Add Firebase + Authentication Google + FB with Firebase UI

--- app/src/main/java/com/dimitri/remoiville/go4lunch/MainActivity.java
+++ app/src/main/java/com/dimitri/remoiville/go4lunch/MainActivity.java
@@ -4,11 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.firebase.ui.auth.AuthUI;
+
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startSignIn();
+    }
+
+
+    private void startSignIn() {
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(Arrays.asList(
+                                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                                new AuthUI.IdpConfig.FacebookBuilder().build()))
+                        .setIsSmartLockEnabled(false,true)
+                        .setTheme(R.style.AuthTheme)
+                        .build(),
+                RC_SIGN_IN);
     }
 }
