c1c509e0cd65319c01ae51e3cf34bbe65ee08b9e
dimitriRemoiville <dimitri.remoiville@gmail.com>
2020-09-23 20:37:50 +1000

    Add layout for DetailsPlaceActivity + color change + rename drawable

---
+++ app/src/main/java/com/dimitri/remoiville/go4lunch/ui/DetailsPlaceActivity.java
@@ -0,0 +1,16 @@
+package com.dimitri.remoiville.go4lunch.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.dimitri.remoiville.go4lunch.R;
+
+public class DetailsPlaceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details_place);
+    }
+}
