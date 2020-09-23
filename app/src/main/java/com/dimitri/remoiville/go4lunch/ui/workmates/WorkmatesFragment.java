90ff8ef1091d381a6e35fe50f46aa54fb8eaecb5
dimitriRemoiville <dimitri.remoiville@gmail.com>
2020-09-23 18:37:11 +1000

    Add layout and RecyclerView for the WorkmatesFragment

--- app/src/main/java/com/dimitri/remoiville/go4lunch/ui/workmates/WorkmatesFragment.java
+++ app/src/main/java/com/dimitri/remoiville/go4lunch/ui/workmates/WorkmatesFragment.java
@@ -1,34 +1,51 @@
 package com.dimitri.remoiville.go4lunch.ui.workmates;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.dimitri.remoiville.go4lunch.R;
+import com.dimitri.remoiville.go4lunch.model.Workmate;
+import com.dimitri.remoiville.go4lunch.ui.listview.ListViewRecyclerViewAdapter;
+
+import java.util.List;
 
 public class WorkmatesFragment extends Fragment {
     private WorkmatesViewModel mWorkmatesViewModel;
+    private RecyclerView mRecyclerView;
+    private Context mContext;
+    private List<Workmate> mWorkmates;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         mWorkmatesViewModel =
                 ViewModelProviders.of(this).get(WorkmatesViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_workmates, container, false);
-        final TextView textView = root.findViewById(R.id.text_workmates);
-        mWorkmatesViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        View root = inflater.inflate(R.layout.fragment_workmates_list, container, false);
+
+        mContext = root.getContext();
+        mRecyclerView = (RecyclerView) root;
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(mContext));
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(mContext, DividerItemDecoration.HORIZONTAL));
+
         return root;
+    }
+
+    private void initList(List<Workmate> workmates) {
+        mRecyclerView.setAdapter(new WorkmatesRecyclerViewAdapter(workmates));
     }
 }
---
+++ app/src/main/java/com/dimitri/remoiville/go4lunch/ui/workmates/WorkmatesRecyclerViewAdapter.java
@@ -0,0 +1,46 @@
+package com.dimitri.remoiville.go4lunch.ui.workmates;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.dimitri.remoiville.go4lunch.databinding.FragmentWorkmatesBinding;
+import com.dimitri.remoiville.go4lunch.model.Workmate;
+
+import java.util.List;
+
+public class WorkmatesRecyclerViewAdapter extends RecyclerView.Adapter<WorkmatesRecyclerViewAdapter.ViewHolder> {
+
+    private List<Workmate> mWorkmates;
+
+    public WorkmatesRecyclerViewAdapter(List<Workmate> items) {
+        mWorkmates = items;
+    }
+
+    @NonNull
+    @Override
+    public WorkmatesRecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(FragmentWorkmatesBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull WorkmatesRecyclerViewAdapter.ViewHolder holder, int position) {
+        // TODO
+    }
+
+    @Override
+    public int getItemCount() {
+        return mWorkmates.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private FragmentWorkmatesBinding mFragmentWorkmatesBinding;
+
+        public ViewHolder(FragmentWorkmatesBinding fragmentWorkmatesBinding) {
+            super(fragmentWorkmatesBinding.getRoot());
+            this.mFragmentWorkmatesBinding = fragmentWorkmatesBinding;
+        }
+    }
+}
